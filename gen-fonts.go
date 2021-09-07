1e206614afe7d077a1d624204e96b14b41365194
Sebastien Binet <binet@cern.ch>
2021-09-07 16:33:15 +0200

    all: use go:embed directive
    Fixes #1.

--- gen-fonts.go
+++ gen-fonts.go
@@ -148,15 +148,10 @@ func do(ttfName string, src []byte) error {
 	fmt.Fprintf(b, "// Package %s provides the %q TrueType font\n", pkgName, fontName)
 	fmt.Fprintf(b, "// from the Liberation font family.\n")
 	fmt.Fprintf(b, "package %[1]s // import \"github.com/go-fonts/liberation/%[1]s\"\n\n", pkgName)
+	fmt.Fprintf(b, "import _ \"embed\"\n")
 	fmt.Fprintf(b, "// TTF is the data for the %q TrueType font.\n", fontName)
-	fmt.Fprintf(b, "var TTF = []byte{")
-	for i, x := range src {
-		if i&15 == 0 {
-			b.WriteByte('\n')
-		}
-		fmt.Fprintf(b, "%#02x,", x)
-	}
-	fmt.Fprintf(b, "\n}\n")
+	fmt.Fprintf(b, "//\n//go:embed %s\n", ttfName)
+	fmt.Fprintf(b, "var TTF  []byte\n")
 
 	dst, err := format.Source(b.Bytes())
 	if err != nil {
@@ -166,6 +161,11 @@ func do(ttfName string, src []byte) error {
 	err = ioutil.WriteFile(filepath.Join(pkgName, "data.go"), dst, 0666)
 	if err != nil {
 		return fmt.Errorf("could not write package source file: %w", err)
+	}
+
+	err = ioutil.WriteFile(filepath.Join(pkgName, ttfName), src, 0666)
+	if err != nil {
+		return fmt.Errorf("could not write package TTF file: %w", err)
 	}
 
 	return nil
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/go-fonts/liberation
 
-go 1.13
+go 1.16
 
 require golang.org/x/image v0.0.0-20200430140353-33d19683fad8
